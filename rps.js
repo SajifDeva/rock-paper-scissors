ddb5ffe01cb748808692a6e89954dddf72ee1f37
Sajif <sajifdevapalan@outlook.com>
2023-06-26 23:52:46 +0100

    rps.js

--- rps.js
+++ rps.js
@@ -15,3 +15,12 @@ function getComputerChoice(){
     }
 
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
