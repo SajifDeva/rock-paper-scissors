74a154ef5e945013749e44edfb2c29d055e86783
Sajif <sajifdevapalan@outlook.com>
2023-06-27 23:13:04 +0100

    rps.js

--- rps.js
+++ rps.js
@@ -1,35 +1,47 @@
-const playerText = document.querySelector("playerText");
-const computerText = document.querySelector("computerText");
-const resultText = document.querySelector("resultText");
-const choiceBtns = document.querySelector(".choiceBtn")
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
 let player;
 let computer;
 let result;
 
+choiceBtns.forEach(button => button.addEventListener("click", () => {
 
-function getComputerChoice(){
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+}));
+
+function computerTurn(){
 
     const randNum = Math.floor(Math.random() * 3) + 1;
 
     switch(randNum){
-        case 1 :
-            computer = "ROCK";
-            break;
-        case 2 :
-            computer = "PAPER";
-            break;
-        case 3 :
-            computer = "SCISSORS";
-            break; 
+      case 1:
+        computer = "ROCK";
+        break;
+      case 2:
+        computer = "PAPER";
+        break;
+      case 3:
+        computer = "SCISSORS";
+        break;
     }
-
 }
-
-
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+function checkWinner(){
+    if(player == computer){
+      return "Draw!";
+    }
+    else if(computer == "ROCK"){
+      return (player == "PAPER") ? "You Win!" : "You Lose!"
+    }
+    else if(computer == "PAPER"){
+      return (player == "SCISSORS") ? "You Win!" : "You Lose!"
+    }
+    else if(computer == "SCISSORS"){
+      return (player == "ROCK") ? "You Win!" : "You Lose!"
+    }
+}
